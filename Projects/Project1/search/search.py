7e69d9b6e77a097f085c040106cd69c0d3c10931
Arshavir Karapetyan <ariko1993@berkeley.edu>
2019-09-08 21:29:19 -0700

    Add

--- Projects/Project1/search/search.py
+++ Projects/Project1/search/search.py
@@ -131,7 +131,7 @@ def uniformCostSearch(problem):
         if curr[0] not in visited:
             visited.append(curr[0])
             for successor in problem.getSuccessors(curr[0]):
-                fringe.push((successor[0], curr[1] + [successor[1]], curr[2] + successor[2]))
+                fringe.push((successor[0], curr[1] + [successor[1]], curr[2] + successor[2]), curr[2] + successor[2])
     util.raiseNotDefined()
 
 def nullHeuristic(state, problem=None):
